a695cb4d451a6aaaa49abf24b6c1fa836676fb8c
zhhlxh <13675132313@163.com>
2019-03-28 13:05:48 +0800

    新建另外三个产品编辑页面

---
+++ GYH/src/module/p/edit_file/edit.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './App'
+import appsettings from 'common/appsettings.js'
+
+Vue.use(appsettings)
+/* eslint-disable no-new */
+new Vue({el: '#app',template: '<App/>', components: { App }})
